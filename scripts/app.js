e7a2ea76dd64436c3845acdfbdfde5a802284d6c
morganjaouni <morgan.jaouni@ttu.edu>
2020-09-22 13:41:27 -0500

    added scoreboard and styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,56 @@
-console.log('PROJECT0')
+
+//Create a 'scoreboard' for the game including name,age,hunger,boredom
+//Include 'begin' button
+//style with css
+//create an alert that welcomes user to game TODO: allow user input to name the snail 
+// alert('WELCOME TO TOMAGOTCHI! NAME YOUR PET SNAIL...FEED IT TO LOWER HUNGER (1-10)....& PET IT TO KEEP IT FROM DYING OF BOREDOM..');
+
+//Create 'gary' the snail class
+class Gary {
+        name = 'Gary';
+        age = 1;
+        hunger = 1;
+        boredom = 1;
+}
+
+
+// Instatiate your Tomagotchi
+
+
+
+
+
+
+
+
+// Display a character of your choice on the screen to represent your pet
+
+
+
+
+
+
+
+
+
+// Display the following metrics for your pet:
+
+
+
+
+
+
+
+
+// Hunger (1-10 scale)
+// Sleepiness (1-10 scale)
+// Boredom (1-10 scale)
+// Age
+// Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
+// Add the ability to name your pet.
+// Style the page.
+// Increase your pet's age every x minutes
+// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
+// Morph your pet at certain ages.
+// Animate your pet across the screen while it's alive.
